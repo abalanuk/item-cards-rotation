e8bf32342e3b2d9f41710ba536f73e2251de4919
Andrii Balaniuk <andrii.balaniuk@digicert.com>
2022-05-06 20:38:58 +0300

    Add basic UI with stop/start process buttons, add logic for rotating cards according to the task's first part

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState(["A", "B", "C", "D"]);
+  const [timerId, setTimerId] = useState(null);
+  let reordered;
+  const reorderInRealTime = (input) => {
+    setTimeout(function runner(param = input) {
+      reordered = reorderFn(param);
+      setItems(reordered);
+      const timerId = setTimeout(() => runner(reordered), 2000);
+      setTimerId(timerId)
+    }, 2000);
+  }
+  const reorderFn = (arr) => {
+    return [...arr.slice(1, arr.length), arr[0]];
+  }
+
+  const onChange = (event) => {
+    console.log('Input:value', event.target.value)
+  }
+  const handleStopProcess = () => {
+    clearTimeout(timerId);
+  }
+  const handleStartProcess = () => {
+    reorderInRealTime(items);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className='button' onClick={handleStartProcess}>Start process</button>
+        <input type="text" className="test-input" onChange={onChange}/>
+        <div className="card-container">
+          {items.map((item) => <p key={item} className='card-item'>{item}</p>)}
+        </div>
+        <button className='button' onClick={handleStopProcess}>Stop process</button>
       </header>
     </div>
   );
